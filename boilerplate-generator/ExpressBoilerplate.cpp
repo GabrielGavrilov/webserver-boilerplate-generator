d8911de7d9ca030b58a03f8bb675b42ce87cf932
Gabriel Gavrilov <79603829+GabrielGavrilov@users.noreply.github.com>
2022-05-18 07:30:45 -0700

    initial commit

---
+++ boilerplate-generator/ExpressBoilerplate.cpp
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <direct.h>
+
+using namespace std;
+
+class ExpressBoilerplate {
+	public:
+
+		void createExpressBoilerplateFiles(string boilerplateLocation) {
+
+			string appFileLocationString, viewsFolderLocationString, publicFolderLocationString;
+			const char* viewsFolderLocation;
+			const char* publicFolderLocation;
+			fstream fs;
+
+			appFileLocationString = boilerplateLocation + "/app.js";
+			viewsFolderLocationString = boilerplateLocation + "/views/";
+			publicFolderLocationString = boilerplateLocation + "/public/";
+
+			viewsFolderLocation = viewsFolderLocationString.c_str();
+			publicFolderLocation = publicFolderLocationString.c_str();
+
+			if (_mkdir(viewsFolderLocation) == -1) {
+				std::cout << "There has been an issue creating the views folder." << endl;
+			}
+			else {
+				std::cout << "-- Views folder created..." << endl;
+			}
+
+			if (_mkdir(publicFolderLocation) == -1) {
+				std::cout << "There has been an issue creating the public folder." << endl;
+			}
+			else {
+				std::cout << "-- Public folder created..." << endl;
+			}
+
+			fs.open(appFileLocationString, std::fstream::in | std::fstream::out | std::fstream::trunc);
+			fs << "\n";
+			fs.close();
+			std::cout << "-- App file created..." << endl;
+
+		}
+
+		void updateExpressBoilerplateFiles(string boilerplateLocation) {
+ 
+			string appFileLocationString, textFileLocationString, line;
+			fstream textStream;
+			fstream appStream;
+
+			appFileLocationString = boilerplateLocation + "/app.js";
+			textFileLocationString = "express.txt";
+
+			textStream.open(textFileLocationString, std::fstream::in | std::fstream::out | std::fstream::app);
+			appStream.open(appFileLocationString, std::fstream::in | std::fstream::out | std::fstream::app);
+
+			while (getline(textStream, line)) {
+				appStream << line << endl;
+			}
+
+		}
+
+};
---
+++ boilerplate-generator/Main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <direct.h>
+#include "ExpressBoilerplate.cpp"
+
+using namespace std;
+
+void chooseExpressOption() {
+
+	ExpressBoilerplate e;
+
+	string folderLocationString;
+	const char* folderLocation;
+
+	std::cout << "\nEnter location where express boilerplate is to be created: ";
+	std::cin >> folderLocationString;
+
+	folderLocation = folderLocationString.c_str();
+	
+	if (_mkdir(folderLocation) == -1) {
+		std::cout << "There has been an issue creating the express boilerplate folder." << endl;
+	}
+	else {
+		std::cout << "\n-- Express boilerplate folder created..." << endl;
+		e.createExpressBoilerplateFiles(folderLocationString);
+		e.updateExpressBoilerplateFiles(folderLocationString);
+	}
+
+}
+
+int main() {
+
+	string inputString;
+	int inputInt;
+
+	std::cout << "\nWeb-server boilerplate generator\n\n" <<
+		"  [1] : Node.js (with Express.js)\n" <<
+		"  [2] : Python Flask\n\n"
+		<< "Choose language: ";
+	std::cin >> inputString;
+
+	inputInt = std::stoi(inputString);
+
+	switch (inputInt) {
+		case 1:
+			chooseExpressOption();
+	}
+
+	return 0;
+}
